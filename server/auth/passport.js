b6a61273265b1685ef194bf30831b103331330f8
Ruksachai Onglaor <Ruksachai@gmail.com>
2020-12-05 17:49:20 +0700

    add verifyUserFunction

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -5,13 +5,19 @@ import config from "../config";
 import { User } from "../../Model/";
 
 passport.use(
-	new OAuth2Strategy({
-		authorizationURL: config.OAUTH.AUTHORIZATION_ENDPOINT,
-		tokenURL: config.OAUTH.TOKEN_ENDPOINT,
-		clientID: config.OAUTH.CLIENT_ID,
-		clientSecret: config.OAUTH.CLIENT_SECRET,
-		callbackURL: config.OAUTH.REDIRECT_URL,
-	})
+	new OAuth2Strategy(
+		{
+			authorizationURL: config.OAUTH.AUTHORIZATION_ENDPOINT,
+			tokenURL: config.OAUTH.TOKEN_ENDPOINT,
+			clientID: config.OAUTH.CLIENT_ID,
+			clientSecret: config.OAUTH.CLIENT_SECRET,
+			callbackURL: config.OAUTH.REDIRECT_URL,
+		},
+		function (accessToken, refreshToken, profile, done) {
+			console.log(accessToken, refreshToken, profile);
+			done();
+		}
+	)
 );
 
 app.use(passport.initialize());
