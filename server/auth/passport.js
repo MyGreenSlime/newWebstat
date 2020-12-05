5a0d5fc859c5f68422fcba9903a00e7bfcb0ccc3
Ruksachai Onglaor <Ruksachai@gmail.com>
2020-12-05 17:24:12 +0700

    create deploy script

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -5,22 +5,24 @@ import config from "../config";
 import { User } from "../../Model/";
 
 passport.use(
-	new OAuth2Strategy(
-		{
-			authorizationURL: config.OAUTH.AUTHORIZATION_ENDPOINT,
-			tokenURL: config.OAUTH.TOKEN_ENDPOINT,
-			clientID: config.OAUTH.CLIENT_ID,
-			clientSecret: config.OAUTH.CLIENT_SECRET,
-			callbackURL: config.OAUTH.REDIRECT_URL,
-		},
-		async function (accessToken, refreshToken, profile, done) {
-			console.log(accessToken, refreshToken, profile, done);
-			done();
-		}
-	)
+	new OAuth2Strategy({
+		authorizationURL: config.OAUTH.AUTHORIZATION_ENDPOINT,
+		tokenURL: config.OAUTH.TOKEN_ENDPOINT,
+		clientID: config.OAUTH.CLIENT_ID,
+		clientSecret: config.OAUTH.CLIENT_SECRET,
+		callbackURL: config.OAUTH.REDIRECT_URL,
+	})
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get("/auth/", passport.authenticate("oauth2"));
+
+app.get(
+	"/auth/callback",
+	passport.authenticate("oauth2", { failureRedirect: "/loginfailure" }),
+	(req, res) => {
+		res.send(req.query);
+	}
+);
